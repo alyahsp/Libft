4e9101420798226f2b8a93ead1d7d2f3df6df200
Stanya Palmaro <spalmaro@41.student.fr>
2016-11-05 03:15:37 +0100

    made a pointer to copy value of content

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -15,15 +15,21 @@
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*list;
+	void	*cpy;
 
-	if (!(list = (t_list *)malloc(sizeof(t_list))))
+	cpy = ft_memalloc(content_size);
+	if (!cpy)
 		return (NULL);
 	if (!content)
 	{
-		content = NULL;
+		cpy = NULL;
 		content_size = 0;
 	}
-	ft_memcpy(list->content, content, content_size);
+	else
+		cpy = ft_memcpy(cpy, content, content_size);
+	if (!(list = (t_list *)malloc(sizeof(t_list))))
+		return (NULL);
+	list->content = cpy;
 	list->content_size = content_size;
 	list->next = NULL;
 	return (list);
